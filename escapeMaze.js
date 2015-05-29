7028ae17c6f4f86504102325c5f0791cf711ed0f
guanbeiqi@gmail.com <guanbeiqi@jiuzhilan.net>
2015-05-29 03:05:22 -0700

    cube function added

--- escapeMaze.js
+++ escapeMaze.js
@@ -4,6 +4,107 @@ var program;
 var vPosition;
 var vNormal;
 
+
+// CubeObject
+var myCube = {
+    ambient : vec4(1.0, 1.0, 1.0, 1.0),
+    diffuse : vec4(1.0, 1.0, 1.0, 1.0),
+    specular : vec4(1.0, 1.0, 1.0, 1.0),
+    shininess : 30.0,
+    center : 1,
+    vertices :
+    [
+    vec4( -0.5, -0.5,  0.5, 1.0 ), //left bot front 0
+    vec4( -0.5,  0.5,  0.5, 1.0 ), //left top front 1
+    vec4( 0.5,  0.5,  0.5, 1.0 ), //right top front 2
+    vec4( 0.5, -0.5,  0.5, 1.0 ), //right bot front 3
+    vec4( -0.5, -0.5, -0.5, 1.0 ), //left bot back 4
+    vec4( -0.5,  0.5, -0.5, 1.0 ), //left top back 5
+    vec4( 0.5,  0.5, -0.5, 1.0 ), //right top back 6
+    vec4( 0.5, -0.5, -0.5, 1.0 ) //right bot back 7
+    ],
+    vertexNum: 36,
+    pointsArray : [],
+    normalsArray : [],
+    ambientProduct : vec4(),
+    diffuseProduct : vec4(),
+    specularProduct : vec4(),
+    modelMatrix : mat4(),
+    modelViewMatrix : mat4(),
+
+}
+
+//functions
+myCube.init = function() {
+            //call helper function to generate cube
+            drawCube();
+
+            //create buffers for sphere
+            myCube.vBuffer = gl.createBuffer();
+            myCube.nBuffer = gl.createBuffer();
+
+            //-----------------------------------------push sphere vertices in buffer--------------------------------------
+
+                gl.bindBuffer(gl.ARRAY_BUFFER,myCube.vBuffer);
+                gl.bufferData(gl.ARRAY_BUFFER,flatten(myCube.pointsArray),gl.STATIC_DRAW);
+
+                //link and enable position attribute
+
+                gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+                gl.enableVertexAttribArray( vPosition );
+                //unlink buffer
+                gl.bindBuffer(gl.ARRAY_BUFFER,null);
+
+                ////-----------------------------------------push normal vector in buffer--------------------------------------
+                gl.bindBuffer(gl.ARRAY_BUFFER,myCube.nBuffer);
+                gl.bufferData(gl.ARRAY_BUFFER,flatten(myCube.normalsArray),gl.STATIC_DRAW);
+
+                //link and enable normal attribute
+
+                gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+                gl.enableVertexAttribArray( vNormal);
+                //unlink buffer
+                gl.bindBuffer(gl.ARRAY_BUFFER,null);
+        };
+
+myCube.draw = function() {
+                // bind ball's buffer
+                gl.bindBuffer( gl.ARRAY_BUFFER, myCube.vBuffer );
+                gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
+                gl.bindBuffer( gl.ARRAY_BUFFER, myCube.nBuffer );
+                gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+
+                 //transfer values
+                gl.uniform4fv( loc.ambientProductLoc,flatten(myCube.ambientProduct) );
+                gl.uniform4fv( loc.diffuseProductLoc,flatten(myCube.diffuseProduct) );
+                gl.uniform4fv( loc.specularProductLoc,flatten(myCube.specularProduct) );
+                gl.uniform4fv( loc.lightPositionLoc, flatten(light.position) );
+                gl.uniform1f( loc.shininessLoc, mySphere.shininess );
+                gl.uniform1i( loc.isPhongLoc, mySphere.isPhong );
+
+
+                //intergrate modelview matrix
+                myCube.modelViewMatrix = mult(camera.viewMatrix, myCube.modelMatrix);
+                myCube.normalMatrix =  transpose(invert4(myCube.modelViewMatrix));
+                light.modelViewMatrix = mult(camera.viewMatrix, light.modelMatrix);
+
+                // transfer data
+                //sphere
+                gl.uniformMatrix4fv(loc.modelViewMatrixLoc, false, flatten(myCube.modelViewMatrix) );
+                gl.uniformMatrix4fv(loc.normalMatrixLoc, false, flatten(myCube.normalMatrix) );
+
+                //light
+                gl.uniformMatrix4fv(loc.lightModelViewMatrixLoc, false, flatten(light.modelViewMatrix));
+                gl.uniform4fv(loc.lightPositionLoc, flatten(light.position));
+
+                //projection matrix
+                gl.uniformMatrix4fv(loc.projectionMatrixLoc, false, flatten(camera.projectionMatrix) );
+
+                //draw cube
+                gl.drawArrays( gl.TRIANGLES, 0, myCube.vertexNum );
+
+            };
+
 // Sphere Object
 
 var mySphere = {
@@ -35,6 +136,8 @@ var mySphere = {
     */
 
 };
+
+
 //functions
 mySphere.init = function() {
             //call helper function to generate sphere
@@ -229,7 +332,7 @@ var camera = {
     aspect : 0 ,  // will be overwritten in init
     near : 0.3,
     far : 300.0,
-    viewMatrix : mat4(),
+    viewMatrix : mult(rotate(90, [1,0,0]), translate(0, -10, 0)),
     projectionMatrix : mat4()
 };
 
@@ -249,6 +352,8 @@ loc.getAllUniformLoc = function(){
     loc.isPhongLoc =  gl.getUniformLocation(program, "isPhong");
 
 }
+
+/**** Sphere Related Drawing Funcitons ****/
 
 
 function triangle(a, b, c, n) {
@@ -334,6 +439,98 @@ function generateSpheres(){
     mySphere.specularProduct = mult(mySphere.specular, light.specular);
 }
 
+function quad(a, b, c, d) {
+
+    //no texture for now 
+
+     myCube.pointsArray.push(myCube.vertices[a]); 
+     // myCube.colorsArray.push(myCube.color); 
+     // texCoordsArray.push(texCoord[0]);
+
+     myCube.pointsArray.push(myCube.vertices[b]); 
+     // myCube.colorsArray.push(myCube.color);
+     // texCoordsArray.push(texCoord[1]); 
+
+     myCube.pointsArray.push(myCube.vertices[c]); 
+     // myCube.colorsArray.push(myCube.color);
+     // texCoordsArray.push(texCoord[2]); 
+   
+     myCube.pointsArray.push(myCube.vertices[a]); 
+     // myCube.colorsArray.push(myCube.color);
+     // texCoordsArray.push(texCoord[0]); 
+
+     myCube.pointsArray.push(myCube.vertices[c]); 
+     // myCube.colorsArray.push(myCube.color);
+     // texCoordsArray.push(texCoord[2]); 
+
+     myCube.pointsArray.push(myCube.vertices[d]); 
+     // myCube.colorsArray.push(myCube.color);
+     // texCoordsArray.push(texCoord[3]);   
+}
+
+function storeNormal(face) {
+
+    //six points per face
+    switch (face)
+    {
+        case 'front':
+        //normal facing upwards
+        for (var i = 0; i < 6; i++)
+            myCube.normalsArray.push(vec4(0,0,-1,0));
+
+        break;
+
+        case 'back':
+        for (var i = 0; i < 6; i++)
+            myCube.normalsArray.push(vec4(0,0,1,0));
+        break;
+
+        case 'left':
+        for (var i = 0; i < 6; i++)
+            myCube.normalsArray.push(vec4(-1,0,0,0));
+        break;
+
+        case 'right':
+        for (var i = 0; i < 6; i++)
+            myCube.normalsArray.push(vec4(1,0,0,0));
+        break;
+
+        case 'top':
+        for (var i = 0; i < 6; i++)
+            myCube.normalsArray.push(vec4(0,1,0,0));
+        break;
+
+        case 'bottom':
+        for (var i = 0; i < 6; i++)
+            myCube.normalsArray.push(vec4(0,-1,0,0));
+        break;
+
+    }
+}
+
+function drawCube() {
+
+    quad( 1, 0, 3, 2 ); //1 0 3 1 3 2 //front face
+    storeNormal('front');
+
+    quad( 2, 3, 7, 6 ); // 2 3 7 2 7 6 //right face
+    storeNormal('right');
+
+    quad( 4, 0, 3, 7 ); // 3 0 4 3 4 7 //bottom face //4 0 3 4 3 7
+    storeNormal('bottom');
+
+    quad( 5, 1, 2, 6 ); // 6 5 1 6 1 2 //top face //5 1 2 5 2 6
+    storeNormal('top');
+
+    quad( 6, 7, 4, 5 ); // 4 5 6 4 6 7 //back face //6 7 4 6 4 5
+    storeNormal('back');
+
+    quad( 5, 4, 0, 1 ); // 5 4 0 5 0 1 //left face //
+    storeNormal('left');
+
+}
+
+/**** init function called when window loads ****/
 
 window.onload = function init() {
     //web GL setup
@@ -355,10 +552,13 @@ window.onload = function init() {
 
     vPosition = gl.getAttribLocation( program, "vPosition" );
     vNormal = gl.getAttribLocation( program, "vNormal" );
+    // vColor = gl.getAttribLocation(program, "vColor");
 
     //init all objects
+    myCube.init();
     mySphere.init();
     basePlane.init();
+
 
 
     //get Locs
@@ -382,6 +582,7 @@ function render(){
 
     mySphere.draw();
     basePlane.draw();
+    myCube.draw();
 
     setTimeout(
         function (){requestAnimFrame(render);}, 1000/60
